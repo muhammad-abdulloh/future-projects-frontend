96436cbbf58496b49a249d18fe0d5d8d5d8b867f
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-04-15 12:09:51 +0500

    angular crud lesson

---
+++ src/app/serices/crud.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  baseUrl: string = "https://localhost:44396/api/Users/GetAllUsers";
+
+  constructor(private http : HttpClient) { }
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl);
+  }
+
+}
