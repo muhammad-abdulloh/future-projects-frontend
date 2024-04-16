2954ae0b1be4dd30f178a2cf8a5c91fc11b3b414
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-04-16 10:54:15 +0500

    create user with ts

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../serices/crud.service';
+import { CreateUser } from '../../models/createUser';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
+
+  resultData! : CreateUser;
+
+  setValue: CreateUser = {
+    name: "Akramjon",
+    email: "akramjon47@calachnikov.ru",
+    password: "sadsad",
+    login: "Admin11",
+    role: "Student"
+  }
+
+  constructor(private crudService: CrudService) {
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  createUser(data: CreateUser) {
+     this.crudService.create(data).subscribe({
+      next: (result) => {
+        this.resultData = result;
+        console.log(result);
+      },
+      error: (err) => {
+        console.log(`Error ketti: ${err}`);
+      }
+    });
+  }
+
+  setUser() {
+    this.createUser(this.setValue);
+    
+  }
+
+
+
 
 }
--- src/app/serices/crud.service.ts
+++ src/app/serices/crud.service.ts
@@ -2,18 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
+import { CreateUser } from '../models/createUser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  baseUrl: string = "https://localhost:44396/api/Users/GetAllUsers";
+  baseUrl: string = "https://localhost:44396/api/Users/";
 
   constructor(private http : HttpClient) { }
 
   getAll(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl);
+    return this.http.get<User[]>(this.baseUrl + 'GetAllUsers');
+  }
+
+  create(data: CreateUser): Observable<CreateUser> {
+    return this.http.post<CreateUser>(this.baseUrl + 'CreateUser', data)
   }
 
 }
