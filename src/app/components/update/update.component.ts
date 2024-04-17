4052f00a2e62c79073edc8bc60b3087329f9b93a
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-04-17 11:07:11 +0500

    update and getById

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CreateUser } from '../../models/createUser';
+import { CrudService } from '../../serices/crud.service';
 
 @Component({
   selector: 'app-update',
@@ -6,5 +8,60 @@ import { Component } from '@angular/core';
   styleUrl: './update.component.scss'
 })
 export class UpdateComponent {
+  myId!: number;
+  user: CreateUser = {
+    name: "",
+    email: "",
+    password: "",
+    login: "",
+    role: ""
+  };
+
+  setValue: CreateUser = {
+    name: "",
+    email: "",
+    password: "",
+    login: "",
+    role: ""
+  };
+
+  constructor(private http : CrudService){
+    this.getById();
+  }
+
+  ngOnInit(): void {
+
+    this.getById();
+
+  }
+
+
+
+  getById(){
+
+    this.http.getById(this.myId).subscribe({
+      next: (data) => {
+        this.user = data;
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+
+  }
+
+  setUser(){
+    this.http.update(this.myId, this.setValue).subscribe({
+      next: (data) => {
+        this.user = data;
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
 
 }
--- src/app/serices/crud.service.ts
+++ src/app/serices/crud.service.ts
@@ -21,4 +21,16 @@ export class CrudService {
     return this.http.post<CreateUser>(this.baseUrl + 'CreateUser', data)
   }
 
+  getById(id: number): Observable<CreateUser> {
+    return this.http.get<CreateUser>(this.baseUrl + `UserGetById?id=${id}`)
+  }
+
+  update(id: number, data: CreateUser): Observable<CreateUser> {
+    return this.http.put<CreateUser>(this.baseUrl + `UpdateUser?id=${id}`, data)
+  }
+
+  delete(id: number): Observable<boolean> {
+    return this.http.delete<boolean>(this.baseUrl + `DeleteUserById?id=${id}`)
+  }
+
 }
